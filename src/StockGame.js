cc97d63ea8119908d46f705a5b78daffc6a7e9b9
Kai <deweykai5@gmail.com>
2022-03-05 14:02:16 -0700

    stock data look better

--- src/StockGame.js
+++ src/StockGame.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ControlPanel } from "./ControlPanel";
 import { Timeline } from "./Timeline";
 
+import rawData from "./data";
+import { parseCsv } from "./csvParser";
+
 export function StockGame() {
+    let [stockData, setStockData] = useState([]);
+
+    useEffect(() => {
+        setStockData(parseCsv(rawData));
+    });
+
     return (
         <>
-            <Timeline />
+            <Timeline stockData={stockData} />
             <ControlPanel />
         </>
     );
--- src/Timeline.js
+++ src/Timeline.js
@@ -1,7 +1,11 @@
 import React from "react";
-import data from "./data";
-import { parseCsv } from "./csvParser";
 
-export function Timeline() {
-    return <h1>{parseCsv(data)}</h1>;
+export function Timeline({ stockData }) {
+    //return <h1>{parseCsv(data)}</h1>;
+    return (
+        <>
+            <h1>Timeline</h1>
+            <p>{stockData.length}</p>
+        </>
+    );
 }
