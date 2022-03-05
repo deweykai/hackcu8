d0b197df5d6f22774b5fe1888b51c0898da1b2fa
gal <gweitz17@gmail.com>
2022-03-05 14:21:51 -0700

    updated player class

--- src/playerclass.js
+++ src/playerclass.js
@@ -4,26 +4,3 @@ export class Player {
     this.wallet = 1000;
     this.shares = 0;
   }
-  // Getter
-  get money() {
-    return this.wallet;
-  }
-
-  get shares() {
-    return this.shares;
-  }
-
-  // Setters
-
-  // Method
-  calcArea() {
-    return this.height * this.width;
-  }
-}
-
-let player = new Player();
-let money = player.wallet;
-
-const square = new Rectangle(10, 10);
-
-console.log(square.area); // 100
