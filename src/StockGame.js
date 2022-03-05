5c92f5e2b11a2dabc42ffb992887c446edd89770
Kai <deweykai5@gmail.com>
2022-03-05 16:42:17 -0700

    game context

--- src/StockGame.js
+++ src/StockGame.js
@@ -6,21 +6,32 @@ import Grid from "@mui/material/Grid";
 import rawData from "./data";
 import { parseCsv } from "./csvParser";
 
+const GameContext = React.createContext({
+    game: new Game(),
+    setGame: () => {},
+});
+
+export { GameContext };
+
 export function StockGame() {
     let [stockData, setStockData] = useState([]);
+    let [game, setGame] = useState(new Game());
+    let value = { game, setGame };
 
     useEffect(() => {
         setStockData(parseCsv(rawData));
     }, []);
 
     return (
-        <Grid container>
-            <Grid item xs={8}>
-                <Timeline stockData={stockData} />
+        <GameContext.Provider value={value}>
+            <Grid container>
+                <Grid item xs={8}>
+                    <Timeline stockData={stockData} />
+                </Grid>
+                <Grid item xs={4}>
+                    <ControlPanel />
+                </Grid>
             </Grid>
-            <Grid item xs={4}>
-                <ControlPanel />
-            </Grid>
-        </Grid>
+        </GameContext.Provider>
     );
 }
--- src/Timeline.js
+++ src/Timeline.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PlayerContext } from "./StockGame";
 
 export function Timeline({ stockData }) {
+    let { player, setPlayer } = useContext(PlayerContext);
+    const click = () => {
+        setPlayer("josh");
+        console.log("click");
+    };
     //return <h1>{parseCsv(data)}</h1>;
     return (
         <>
-            <h1>Timeline</h1>
+            {player}
+            <h1 onClick={click}>Timeline</h1>
             <p>{stockData.length}</p>
         </>
     );
