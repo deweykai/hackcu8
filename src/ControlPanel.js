ea692db351eb4bdc1f3846b34a81ff7b82267216
Oleracea <oleracea@galaxybook.localdomain>
2022-03-06 10:00:24 -0700

    Updates

--- src/ControlPanel.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { StockPanel } from "./StockPanel";
-
-export function ControlPanel() {
-    return <StockPanel />;
-}
--- src/playerclass.js
+++
@@ -1,6 +0,0 @@
-export class Player {
-    constructor() {
-        this.wallet = 1000;
-        this.shares = 0;
-    }
-}
