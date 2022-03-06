f72093cfe948442b8349ca59b6ea6f7eeb112c2f
Kai <deweykai5@gmail.com>
2022-03-05 17:25:44 -0700

    logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import { StockGame } from "./StockGame";
 import "./App.css";
 
 function App() {
     return (
         <div style={{ height: "90vh" }}>
+            <img src="/Logo.png" width={"400px"}></img>
             <StockGame />
         </div>
     );
